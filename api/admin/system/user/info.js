aa2285f479ef210bb8ca8ab6726be2982d8e4bf2
pawel <linpeiwei@f-road.com.cn>
2022-02-18 09:38:12 +0800

    fix2

--- api/admin/system/user/info.js
+++ api/admin/system/user/info.js
@@ -1,12 +1,15 @@
 module.exports = (req, res) => {
     res.json({
-        id: '1',
-        loginName: 'admin',
-        realName: '系统管理员',
-        roleId: '1',
-        orgCode: '11',
-        mobile: '13333333333',
-        status: '0',
-        description: '备注'
+        code: '0000',
+        data: {
+            id: '1',
+            loginName: 'admin',
+            realName: '系统管理员',
+            roleId: '1',
+            orgCode: '11',
+            mobile: '13333333333',
+            status: '0',
+            description: '备注'
+        }
     })
 }
